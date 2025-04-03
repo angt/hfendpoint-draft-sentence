e90b04e99bfeb0cc235522a546e1c575e28d5b56
Adrien Gallouët <adrien@gallouet.fr>
2025-04-03 09:04:25 +0200

    Reply usage
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- sentence.py
+++ sentence.py
@@ -5,16 +5,21 @@ from sentence_transformers import SentenceTransformer
 model_name = 'all-MiniLM-L6-v2'
 device = torch.device("mps") if torch.backends.mps.is_available() and torch.backends.mps.is_built() else torch.device("cpu")
 model = SentenceTransformer(model_name, device=device)
+tokenizer = model.tokenizer
+
 print(f"Model {model_name} loaded on {device}")
 
 def embeddings_handler(request_data, send_chunk):
     try:
-        input_data = request_data.get('input', '')
-        texts = [input_data] if isinstance(input_data, str) else [str(item) for item in input_data]
-        embeddings = [emb.tolist() for emb in model.encode(texts)] if texts else []
+        texts = request_data["input"]
+        tokens = sum(len(tokenizer.encode(t, add_special_tokens=True)) for t in texts)
+        embeddings = model.encode(texts)
         send_chunk({
-            'embeddings': embeddings,
-            'usage': {'prompt_tokens': 0, 'total_tokens': 0}, # Fake
+            'embeddings': [emb.tolist() for emb in embeddings],
+            'usage': {
+                'prompt_tokens': tokens,
+                'total_tokens': tokens
+            },
             'model': model_name
         })
     except Exception as e:
